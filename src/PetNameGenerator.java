732d32f212b8a33284c73e2452e417e1661a442e
Dafali9012 <Daniel.falklimberg@gmail.com>
2020-01-29 16:32:47 +0100

    end of day save

--- src/PetNameGenerator.java
+++ src/PetNameGenerator.java
@@ -1,6 +1,8 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -10,13 +12,14 @@ public class PetNameGenerator {
 
     public PetNameGenerator() {
         loadNames();
+        System.out.println(namesData.get(0));
     }
 
     private void loadNames() {
         try {
             namesData = Files.lines(Paths.get("PetNames.txt"))
-            .peek(s -> System.out.println(s))
             .collect(Collectors.toList());
+            namesData = List.of(namesData.get(0).split(" "));
         } catch (IOException e) { e.printStackTrace(); }
     }
 
